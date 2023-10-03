c72b2ece5563cc8faad8375f18da1d64f6797732
devaaly <dev.alymiller@gmail.com>
2023-10-03 21:02:55 +0100

    Adding also a system to know about the user

--- logic-js-4/script.js
+++ logic-js-4/script.js
@@ -1,75 +1,75 @@
-// Exercice #1- Who is the user
+Exercice #1- Who is the user
 
-// let userName = prompt("What is your name?");
-// let userAge = Number(prompt("What is your age"));
-// let userTall = parseFloat(prompt("How tall are you?"));
-// let userWeigth = parseFloat(prompt("What is your weight?"));
-// let userJob = prompt("What is your job?");
+let userName = prompt("What is your name?");
+let userAge = Number(prompt("What is your age"));
+let userTall = parseFloat(prompt("How tall are you?"));
+let userWeigth = parseFloat(prompt("What is your weight?"));
+let userJob = prompt("What is your job?");
 
-// console.log("Hi " + userName + ", you are " + userAge + " years old, you are a " + userJob + ", you are " + userTall + "m tall and weigh " + userWeigth + "kg");
+console.log("Hi " + userName + ", you are " + userAge + " years old, you are a " + userJob + ", you are " + userTall + "m tall and weigh " + userWeigth + "kg");
 
-// if (userAge >= 18) {
-//     console.log("You can drink alcool");
-// } else {
-//     console.log("You can NOT drink alcool");
-// }
-
-
-// let ageM = userAge * 12;
-// let ageW = userAge * 48;
-// let ageD = userAge * 365;
+if (userAge >= 18) {
+    console.log("You can drink alcool");
+} else {
+    console.log("You can NOT drink alcool");
+}
 
 
-// console.log("This is your age in months:" + ageM);
-// console.log("This is your age in weeks:" + ageW);
-// console.log("This is your age in days:" + ageD);
+let ageM = userAge * 12;
+let ageW = userAge * 48;
+let ageD = userAge * 365;
 
 
-// let imc;
+console.log("This is your age in months:" + ageM);
+console.log("This is your age in weeks:" + ageW);
+console.log("This is your age in days:" + ageD);
 
-// function calcIMC(userTall, userWeigth) {
-//     imc = userWeigth / (userTall * userTall);
-//     let imC = parseFloat(imc.toFixed(2));
 
-//     return imC;
-// }
+let imc;
 
-// let resultImc = calcIMC(userTall, userWeigth);
-// let imcLevel ;
+function calcIMC(userTall, userWeigth) {
+    imc = userWeigth / (userTall * userTall);
+    let imC = parseFloat(imc.toFixed(2));
 
-// switch (true) {
-//     case (resultImc <= 18.5):
-//         imcLevel = "Underweight";
-//         break;
-//     case (resultImc > 18.5 && resultImc <= 24.9):
-//         imcLevel = "Normal weight";
-//         break;
-//     case (resultImc > 24.9 && resultImc <= 30):
-//         imcLevel = "Overweight";
-//         break;  
-//     case (resultImc > 30 ):
-//         imcLevel = "Obese"
-//         break;        
+    return imC;
+}
+
+let resultImc = calcIMC(userTall, userWeigth);
+let imcLevel ;
+
+switch (true) {
+    case (resultImc <= 18.5):
+        imcLevel = "Underweight";
+        break;
+    case (resultImc > 18.5 && resultImc <= 24.9):
+        imcLevel = "Normal weight";
+        break;
+    case (resultImc > 24.9 && resultImc <= 30):
+        imcLevel = "Overweight";
+        break;  
+    case (resultImc > 30 ):
+        imcLevel = "Obese"
+        break;        
         
-//     default:
-//         ;
-//         break;
-// }
+    default:
+        ;
+        break;
+}
 
 
-// console.log("This is your IMC: " + resultImc + "kg/m2");
-// console.log(imcLevel)
+console.log("This is your IMC: " + resultImc + "kg/m2");
+console.log(imcLevel)
 
-// let year = 2023;
-// let born = (year - userAge);
+let year = 2023;
+let born = (year - userAge);
 
-// console.log("This is the year you were born: " + born );
+console.log("This is the year you were born: " + born );
 
-// for(let i = born ,  a = 0 ; i && a <= userAge ; i++ , a++){
+for(let i = born ,  a = 0 ; i && a <= userAge ; i++ , a++){
     
-//         console.log(i + " - " + a + " years of life")
+        console.log(i + " - " + a + " years of life")
     
-// }
+}
  
 // Exercice #2 - RH system
 
